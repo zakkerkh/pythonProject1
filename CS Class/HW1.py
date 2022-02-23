1128a30c2d51ce3a4fe183054c106e89ace0465e
zakkerkh <zakariyah.khan@hotmail.com>
2022-02-23 11:59:02 -0500

    init

--- CS Class/HW1.py
+++ CS Class/HW1.py
@@ -3,6 +3,8 @@
 #23/02/2022
 #Hello, how are you doing
 
-print("This is an about me program")
-print("My name is Zakariyah Khan)
-print(")
+print("This is anabout me program")
+print("My name is Zakariyah Khan")
+print("Teacher: Ella Katsman")
+print("Courses:\nScience\nMath\nComputer Science\nCivics & Careers\nSchsool: Maple High\nFavorite Quote: 'Those who say, 'Respect all of the different views and opinions' are wrong. Only the truth is respected- al-Ijabat al-Fasilah'\n")
+print("----\nI\nI\nI\nI\nI\n")
