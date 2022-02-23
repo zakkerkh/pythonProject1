daa34ab5e992b4cee9641b46b21cd5504b80f55e
zakkerkh <zakariyah.khan@hotmail.com>
2022-02-23 13:39:04 -0500

    init

--- CS Class/HW1.py
+++ CS Class/HW1.py
@@ -6,6 +6,21 @@
 print("This is anabout me program")
 print("My name is Zakariyah Khan")
 print("Teacher: Ella Katsman")
-print("Courses:\nScience\nMath\nComputer Science\nCivics & Careers\nSchsool: Maple High\nFavorite Quote: 'Those who say, 'Respect all of the different views and opinions' are wrong. Only the truth is respected- al-Ijabat al-Fasilah'\n")
-print("----\nI\nI\nI\nI\nI\n")
-print("")
+print("Courses:\nScience\nMath\nComputer Science\nCivics & Careers\n"
+      "Schsool: Maple High\n"
+      "Favorite Quote: 'Those who say, 'Respect all of the different views and opinions' are wrong. Only the truth is respected- al-Ijabat al-Fasilah'\n")
+
+print("-----")
+print("|    |")
+print("|    |")
+print("|    |")
+print("|    |")
+print("-----")
+
+print("     xxxxx")
+print("    x     x")
+print("   x _   _ x")
+print("  x  O   O  x")
+print("  x    |    x")
+print("   x   |   x")
+print("   x  __/")
