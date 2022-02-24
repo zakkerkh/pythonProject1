e5bcdc94b036b4c476991d3f0e865edde09a58e6
zakkerkh <zakariyah.khan@hotmail.com>
2022-02-24 10:09:44 -0500

    init

--- Main.py
+++ Main.py
@@ -1,51 +1,29 @@
-import arcade
+# Simple pygame program
 
-# Set constants for the screen size
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+# Import and initialize the pygame library
+import pygame
+pygame.init()
 
-# Open the window. Set the window title and dimensions (width and height)
-arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Example")
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
 
-# Set the background color to white.
-# For a list of named colors see:
-# http://arcade.academy/arcade.color.html
-# Colors can also be specified in (red, green, blue) format and
-# (red, green, blue, alpha) format.
-arcade.set_background_color(arcade.color.WHITE)
+# Run until the user asks to quit
+running = True
+while running:
 
-# Start the render process. This must be done before any drawing commands.
-arcade.start_render()
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-# Draw the face
-x = 300
-y = 300
-radius = 200
-arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
+    # Fill the background with white
+    screen.fill((255, 255, 255))
 
-# Draw the right eye
-x = 370
-y = 350
-radius = 20
-arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
-# Draw the left eye
-x = 230
-y = 350
-radius = 20
-arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+    # Flip the display
+    pygame.display.flip()
 
-# Draw the smile
-x = 300
-y = 280
-width = 120
-height = 100
-start_angle = 200
-end_angle = 350
-arcade.draw_arc_outline(x, y, width, height, arcade.color.BLACK, start_angle, end_angle, 10)
-
-# Finish drawing and display the result
-arcade.finish_render()
-
-# Keep the window open until the user hits the 'close' button
-arcade.run()
+# Done! Time to quit.
+pygame.quit()
