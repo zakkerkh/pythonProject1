575c431e69d9c7d1c114ac82d596c4409640b5d8
zakkerkh <zakariyah.khan@hotmail.com>
2022-02-23 21:56:29 -0500

    init

--- Main.py
+++ Main.py
@@ -1,42 +1,12 @@
-#Address Book
-book = []
+monthconv = {
+    "Jan": "January",
+    "Feb": "Febraury",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+}
 
-rlist = ["Yes", "yes", "y"]
-nrlist = ["No", "no", "n"]
-alist = nrlist + rlist
-
-def contact(name, numb, addr):
-    var = name, numb, addr
-    book.append(var)
-    print(var)
-    print(book)
-    redoing()
-
-def redoing():
-    redo = input("Would you like to add another contact?: ")
-    if redo in rlist:
-        name = input("What is the name?: ").title()
-        numb = input("What is the number: ")
-        addr = input("What is the address?: ").title()
-        contact(name, numb, addr)
-    elif redo in nrlist:
-        print("Thank you!")
-        quit()
-    def invalid():
-        if redo not in alist:
-            print("Invalid Entry")
-            stop = input("Would you like to quit?: ")
-            if stop in nrlist:
-                contact()
-            elif stop in rlist:
-                print("Thank you for using the ultimate contact book!")
-                quit()
-            elif stop not in alist:
-                invalid()
-    invalid()
-
-name = input("What is the name?: ").title()
-numb = input("What is the number: ")
-addr = input("What is the address?: ").title()
-
-contact(name, numb, addr)
+var1 = input("What Month:")
+print(monthconv[var1])
+print(monthconv.get("jing", "Not sehr gut entry"))
--- Side Test.py
+++ Side Test.py
@@ -1,30 +1,5 @@
+var = *
 
-#tesing list functions
-#Address Book
-book = []
+var2 = 3 var 2
 
-name = input("What is the name?: ").title()
-numb = input("What is the number: ")
-addr = input("What is the address?: ").title()
-
-var = name, numb, addr
-print(", ".join(var))
-
-
-
-
-
-
-
-equation = input("operation")
-def multiplication(x,y):
-    while equation == "x":
-        print(x*y)
-    multiplication(int(input("HGFJ"))),(int(input("")))
-
-
-
-#testing git, yet again, again, again
-#testing git again of prior lapious carppious
-#testing on pycharm
-
+print(var2)
