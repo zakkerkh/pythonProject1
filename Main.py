9fbda8a6be9a28b76ab4075b9238279bd3f1adf0
zakkerkh <zakariyah.khan@hotmail.com>
2022-02-23 20:24:59 -0500

    init

--- Main.py
+++ Main.py
@@ -10,23 +10,24 @@ def contact(name, numb, addr):
     book.append(var)
     print(var)
     print(book)
-    def redoing():
-        redo = input("Would you like to add another contact?: ")
-        def invalid():
-            if redo not in alist:
-                print("Invalid Entry")
-                stop = input("Would you like to quit?: ")
-                if stop in nrlist:
-                    redoing()
-                elif stop in rlist:
-                    quit()
-                elif stop not in alist:
-                    invalid()
 
-
+def redoing():
+    redo = input("Would you like to add another contact?: ")
+    def invalid():
+        if redo not in alist:
+            print("Invalid Entry")
+            stop = input("Would you like to quit?: ")
+            if stop in nrlist:
+                redoing()
+            elif stop in rlist:
+                quit()
+            elif stop not in alist:
+                invalid()
+    invalid()
 
 
 contact(input("What is the name?: ").title(), input("What is the number: "), addr = input("What is the address?: ").title())
+redoing()
 
 #testing git, yet again, again, again
 #testing git again of prior lapious carppious
