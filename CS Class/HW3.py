bf938a8c22f2d27f09ba32849d5d5812a3908eb7
zakkerkh <zakariyah.khan@hotmail.com>
2022-02-25 13:51:08 -0500

    init

---
+++ CS Class/HW3.py
@@ -0,0 +1,32 @@
+#Zakariyah. M. J. Khan
+#ICS2O_2122_S2
+#25/02/2022
+#Hello, how are you doing
+
+import math
+#how you are not supposed to do it
+#name = print("Enter your name")
+#print ("hello" name!)
+#print ("each apple costs 0.78 cents")
+#apples = input("Enter the number of apples you wish to buy")
+#apples * 0.78 = cost
+#input("Your total cost", cost)
+
+#How you are supposed to do it
+name = input("Enter your name ")
+print("Hello", name, "!") #You can use "+" instead of the second comma to eliminate to space
+
+print("Each apple costs 0.78 dollars")
+apples = int(input("How much do you wish to buy "))
+apple_cost = apples * 0.78
+print("Your total cost is", apple_cost)
+
+print("Each orange costs 0.64 dollars")
+oranges = int(input("How much do you wish to buy "))
+orange_cost = oranges * 0.64
+print("Your total cost is", orange_cost)
+
+print("Each banana costs 1.21 dollars")
+bananas = int(input("How much do you wish to buy "))
+banana_cost = bananas * 0.64
+print("Your total cost is", banana_cost)
