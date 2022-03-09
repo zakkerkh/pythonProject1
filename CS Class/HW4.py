22efa2ff431f3f94065687d15a546451bf9a1c69
zakkerkh <zakariyah.khan@hotmail.com>
2022-03-09 13:51:13 -0500

    init

---
+++ CS Class/HW4.py
@@ -0,0 +1,10 @@
+#Zakariyah. M. J. Khan
+#ICS2O_2122_S2
+#09/03/2022
+#Hello, how are you doing
+
+#run the program at least 3 times
+import random
+print(random.random())
+print(random.random())
+print(random.random())
