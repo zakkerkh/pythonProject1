af41788b4c9b33ee06c5aae618f39ec937b6da36
zakkerkh <zakariyah.khan@hotmail.com>
2022-04-14 11:54:11 -0400

    init

--- CS Class/HW9.py
+++ CS Class/HW9.py
@@ -87,14 +87,16 @@ entry = int(input("Enter a number between 0-20"))
 count = 0
 times_displayed = 0
 while entry:
-    if 1 > entry > 20:
-        count =+ 1
+    if 1 > entry or entry > 20:
+        count = count + 1
         entry = int(input("Enter a number between 0-20"))
     elif 1 < entry < 20:
-        while count:
-            if count != 0:
-                print(1+times_displayed)
-                count =- 1
-        print("goodjob")
+        while count != -1:
+            print(1+times_displayed)
+            times_displayed = times_displayed + 1
+            count = count - 1
+        print("Goodjob")
+        quit()
+
 
 
