f12008490618ea689236c821c368b558a0793930
zakkerkh <zakariyah.khan@hotmail.com>
2022-04-14 14:04:41 -0400

    init

--- CS Class/HW9.py
+++ CS Class/HW9.py
@@ -36,7 +36,7 @@ while entry:
         print("goodjob")
         quit()
 
-#Question 2a
+#Question 2b
 rand = random.randint(1, 30)
 print("the random number is", rand)
 print("Enter a number larger than", rand)
@@ -118,23 +118,30 @@ while entry:
         quit()
 
 #Question 3d
-entry = int(input("Enter a number between 0-20: "))
+entry = int(input("Enter 1st number between 1-20: "))
+entry2 = int(input("Enter 2nd number between 1-20: "))
 count = 0
-times_displayed = 0
-org_count = 0
-while entry:
-    if 1 > entry or entry > 20:
+store = 0
+sum = 0
+while 1 > entry or entry > 20:
+    entry = int(input("Enter 1st number between 1-20: "))
+while 1 > entry2 or entry2 > 20:
+    entry2 = int(input("Enter 2nd number between 1-20: "))
+    print("stuck")
+
+if 1 < entry < 20 and 1 < entry2 < 20:
+    if entry > entry2:
+        store = entry
+        entry = entry2
+        entry2 = store
+    num = entry2 - entry
+    while count != num:
+        print(entry + count)
+        sum = sum + entry + count
         count = count + 1
-        entry = int(input("Enter a number between 0-20: "))
-    elif 1 < entry < 20:
-        while count != -1:
-            print(1+times_displayed)
-            org_count = org_count + 1 + times_displayed
-            times_displayed = times_displayed + 1
-            count = count - 1
-        print(org_count)
-        print("Goodjob")
-        quit()
+    print(sum)
+
+    print("Farewell")
 
 
 
--- Side Test.py
+++ Side Test.py
@@ -1,19 +1,24 @@
-entry = int(input("Enter 1st number between 0-20: "))
-entry2 = int(input("Enter 1st number between 0-20: "))
+entry = int(input("Enter 1st number between 1-20: "))
+entry2 = int(input("Enter 2nd number between 1-20: "))
 count = 0
-times_displayed = 0
-org_count = 0
-while entry:
-    if 1 > entry or entry > 20:
-        count = count + 1
-        entry = int(input("Enter a number between 0-20: "))
-    elif 1 < entry < 20:
-        while count != -1:
-            print(1+times_displayed)
-            org_count = org_count + 1 + times_displayed
-            times_displayed = times_displayed + 1
-            count = count - 1
-        print(org_count)
-        print("Goodjob")
-        quit()
+store = 0
+sum = 0
+while 1 > entry or entry > 20:
+    entry = int(input("Enter 1st number between 1-20: "))
+while 1 > entry2 or entry2 > 20:
+    entry2 = int(input("Enter 2nd number between 1-20: "))
+    print("stuck")
 
+if 1 < entry < 20 and 1 < entry2 < 20:
+    if entry > entry2:
+        store = entry
+        entry = entry2
+        entry2 = store
+    num = entry2 - entry
+    while count != num:
+        print(entry + count)
+        sum = sum + entry + count
+        count = count + 1
+    print(sum)
+
+    print("Farewell")
