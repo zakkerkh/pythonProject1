71ec4a32c9a32c6610cb5d130e4b98984022d2b7
zakkerkh <zakariyah.khan@hotmail.com>
2022-04-18 14:37:56 -0400

    init

--- CS Class/HW10.py
+++ CS Class/HW10.py
@@ -7,20 +7,18 @@ num1 = int(input("First starting number: "))
 num2 = int(input("Second starting number: "))
 
 print("The necklace is: ")
-print(num1)
-print(num2)
 
 var1 = (num1 + num2) % 10
 var2 = (var1 + num2) % 10
 
-print(var1)
-print(var2)
+ans = str(num1) + str(num2) + str(var1) + str(var2)
 
 while var1 and var2:
     if var1 != num1 or var2 != num2:
         var1 = (var1 + var2) % 10
         var2 = (var1 + var2) % 10
-        print(var1)
-        print(var2)
+        ans += str(var1) + str(var2)
+
     elif var1 == num1 and var2 == num2:
+        print(ans)
         quit()
