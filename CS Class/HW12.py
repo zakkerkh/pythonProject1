b03509fbd4f424e15aa57b14af0bd41c84ddb422
zakkerkh <zakariyah.khan@hotmail.com>
2022-04-25 13:58:33 -0400

    init

--- CS Class/HW12.py
+++ CS Class/HW12.py
@@ -1,8 +1,99 @@
 #Zakariyah. M. J. Khan
 #ICS2O_2122_S2
-#21/04/2022
+#25/04/2022
 #Hello, how are you doing
 
-number = "123"
-print(number +2)
-print(number[1]*3+2)
+#1a
+import random
+
+word1 = input("Enter word")
+word2 = input("Enter word")
+
+print(word1, word2, word2, word1)
+
+#1b
+word1 = input("Enter word")
+word2 = input("Enter word")
+
+print(word1, word2, word2, word1)
+
+yes= input("Enter more words? y/n")
+while yes == "y":
+    word1 = input("Enter word")
+    word2 = input("Enter word")
+
+    print(word1, word2, word2, word1)
+    yes = input("Enter more words? y/n")
+if yes == "n":
+    print("goodbye")
+
+#2a/b
+play = input("Would you like to play: y/n ")
+
+word1 = ""
+while word1 != "end" and play == "y":
+    word1 = input("Enter a word: ")
+    html = input("Enter a format: bold/italics/underline ")
+
+    if html == "italics":
+        print("<i>" + word1 + "</i>")
+    elif html == "bold":
+        print("<b>" + word1 + "</b>")
+    elif html == "underline":
+        print("<u>" + word1 + "</u>")
+
+    word1 = input("Enter a word: ")
+
+print("Goodbye")
+
+#2ba
+name = input("Enter you name: ")
+while name != "end":
+    print("Hello", name + "!")
+    name = input("Enter you name: ")
+
+print("Goodbye! ")
+
+# 2bb/bc
+import random
+name = input("Enter you name: ")
+
+while name != "end":
+    num = random.randint(1, 4)
+    if num == 1:
+        print("Hello", name + "!")
+        name = input("Enter you name: ")
+    if num == 2:
+        print("Greetings", name + "!")
+        name = input("Enter you name: ")
+    if num == 3:
+        print("Good day", name + "!")
+        name = input("Enter you name: ")
+    if num == 4:
+        print("Hope you are having a fabulous day", name + "!")
+        name = input("Enter you name: ")
+    num = random.randint(1, 4)
+
+print("Goodbye! ")
+
+#2d
+name = str.capitalize(input("Enter you name: "))
+
+while name != "End":
+    num = random.randint(1, 4)
+    if num == 1:
+        print("Hello", name + "!")
+        name = input("Enter you name: ")
+    if num == 2:
+        print("Greetings", name + "!")
+        name = input("Enter you name: ")
+    if num == 3:
+        print("Good day", name + "!")
+        name = input("Enter you name: ")
+    if num == 4:
+        print("Hope you are having a fabulous day", name + "!")
+        name = input("Enter you name: ")
+    name = str.capitalize(name)
+    num = random.randint(1, 4)
+
+print("Goodbye! ")
--- Side Test.py
+++ Side Test.py
@@ -1,75 +1,22 @@
-#g
 
-entry = int(input("Enter a 3/4/5 digit number: "))
-while entry != -1:
-# THREE DIGIT
-    while not(-1 and (entry >= 100 and entry <= 999) or (entry >= 1000 and entry <= 9999)):
-        entry = int(input("Enter a 3/4/5 digit number: "))
+import random
+name = str.capitalize(input("Enter you name: "))
 
-    if entry >= 100 and entry <= 999:
-        num3 = entry%10
-        num2 = entry%100//10
-        num1 = entry//100
+while name != "End":
+    num = random.randint(1, 4)
+    if num == 1:
+        print("Hello", name + "!")
+        name = input("Enter you name: ")
+    if num == 2:
+        print("Greetings", name + "!")
+        name = input("Enter you name: ")
+    if num == 3:
+        print("Good day", name + "!")
+        name = input("Enter you name: ")
+    if num == 4:
+        print("Hope you are having a fabulous day", name + "!")
+        name = input("Enter you name: ")
+    name = str.capitalize(name)
+    num = random.randint(1, 4)
 
-        print(num3)
-        print(num2)
-        print(num1)
-
-        sum1 = num1+num2+num3
-
-        print("The sum is:", sum1)
-
-        count = 900
-        starter = 99
-        count1 = 0
-        while count != 0:
-            starter += 1
-
-            var3 = starter % 10
-            var2 = starter % 100 // 10
-            var1 = starter // 100
-
-            sum2 = var1+var2+var3
-            if sum2 == sum1:
-                count1 += 1
-
-            count -= 1
-        print("Count:", count1)
-
-#FOUR DIGIT
-    if entry >= 1000 and entry <= 9990:
-        entry = int(input("Enter a 3/4/5 digit number: "))
-
-        num4 = entry%10
-        num3 = entry%100//10
-        num2 = entry%1000//100
-        num1 = entry//1000
-
-        print(num4)
-        print(num3)
-        print(num2)
-        print(num1)
-
-        sum1 = num1+num2+num3+num4
-
-        print("The sum is:", sum1)
-
-        count = 9000
-        starter = 1000
-        count1 = 0
-        while count != 0:
-            starter += 1
-
-            var4 = starter % 10
-            var3 = starter % 100 // 10
-            var2 = starter % 1000 // 100
-            var1 = starter // 1000
-
-            sum2 = var1+var2+var3+var4
-            if sum2 == sum1:
-                count1 += 1
-
-            count -= 1
-        print("Count:", count1)
-    entry = int(input("Enter a 3/4/5 digit number: "))
-    print("Goodbye")
+print("Goodbye! ")
