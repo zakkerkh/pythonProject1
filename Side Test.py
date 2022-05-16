6c7e5407ebb2914fb8c59f13211a02bc4ec8ebb6
zakkerkh <zakariyah.khan@hotmail.com>
2022-05-16 14:40:01 -0400

    init

--- Side Test.py
+++ Side Test.py
@@ -1,29 +1,3 @@
-entry = "kfgjkdshf"
-work = entry
-del_count = -1
-play = "y"
+entry = input("Enter a 7 character number string:")
+guess = input("Guess what the output will be well all consecutive triplets are removed")
 
-while play == "y":
-    if work[0] == work[1] == work[2]:
-        entry = entry[del_count+3:]
-    else:
-        work = work[1:]
-        del_count += 1
-
-#if len(entry) >= 5:
- #   if (work[0] and work[1] and work[2] and work[3]) == work[4]:
-  #      entry = entry[del_count+5:]
-#elif len(entry) >= 4:
- #   elif (work[0] and work[1] and work[2]) == work[3]:
-  #      entry = entry[del_count+4: ]
-#elif len(entry) >= 3:
- #   elif (work[0] and work[1]) == work[2]:
-  #      entry = entry[del_count+4:]
-#elif len(entry) >= 2:
- #   else:
-  #      work = work[1:]
-   #     del_count += 1
-#elif len(entry) >= 1:
- #   else:
-  #      work = work[1:]
-   #     del_count += 1
