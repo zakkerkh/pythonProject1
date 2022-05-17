500d296d803913a29a556a3880984ca4fe65a3f1
zakkerkh <zakariyah.khan@hotmail.com>
2022-05-17 11:20:41 -0400

    init

--- Side Test.py
+++ Side Test.py
@@ -1,3 +1,46 @@
 entry = input("Enter a 7 character number string:")
-guess = input("Guess what the output will be well all consecutive triplets are removed")
+guess = input("Guess what the output will be all consecutive triplets are removed")
+count = 0
+count1 = 0
+points = 0
+redo = "y"
+list = ""
+
+while redo == "y":
+    while count < 10:
+        if len(entry) >= 3 and entry[0] == entry[1] == entry[2]:
+            entry = entry[3:]
+            count1 += 1
+        elif len(entry) >= 4 and entry[1] == entry[2] == entry[3]:
+            entry = entry[0:1] + entry[4:]
+            count1 += 1
+        elif len(entry) >= 5 and entry[2] == entry[3] == entry[4]:
+            entry = entry[0:2] + entry[5:]
+            count1 += 1
+        elif len(entry) >= 6 and entry[3] == entry[4] == entry[5]:
+            entry = entry[0:3] + entry[6:]
+            count1 += 1
+        elif len(entry) >= 7 and entry[4] == entry[5] == entry[6]:
+            entry = entry[0:4] + entry[7:]
+            count1 += 1
+
+        count += 1
+
+    print(entry)
+    list = list, "entry"
+
+    if guess == entry:
+        print("You got it right")
+        points += 1
+    else:
+        print("You got it wrong")
+
+    redo = input("Would you like to play again? y/n: ")
+    if redo == "y":
+        entry = input("Enter a 7 character number string:")
+        guess = input("Guess what the output will be all consecutive triplets are removed")
+        count = 0
+        count1 = 0
+
+
 
