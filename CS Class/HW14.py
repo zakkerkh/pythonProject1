e0561b88faabd670fc9d9c8d945ce97d6548a076
zakkerkh <zakariyah.khan@hotmail.com>
2022-05-24 14:42:24 -0400

    init

--- CS Class/HW14.py
+++ CS Class/HW14.py
@@ -3,19 +3,47 @@
 #20/05/2022
 #Hello, how are you doing
 
-myList = [3, 4, 8, 11, 15]
-count = 0
+#1
+# myList = [3, 4, 8, 11, 15]
+# count = 0
+# num = 0
+# repeat = True
+#
+# while repeat:
+#     num1 = input("What number would you like to add to the list?: ")
+#     if num1 == "Done":
+#         repeat = False
+#     else:
+#         place = input("Where would you like to add the number?: ")
+#
+#         myList[int(place)-1] = int(num1)
+# print(myList)
+# while count != len(myList):
+#     count += 1
+#     if count % 2 != 0:
+#         print("Item", count, "is:", myList[count-1])
+#         num += myList[count-1]
+#
+# myList = [3, 4, 8, 11, 15]
+# count = 0
+#
+# print(num)
 
-while count != len(myList):
-    count += 1
-    print("Item", count, "is:", myList[count-1])
+#2
+Fruits = ["apple", "pear", "peach"]
 
-myList = [3, 4, 8, 11, 15]
-count = 0
-num = 0
+fruitList = ("The list has the fruits:", Fruits[0].capitalize(), Fruits[1].capitalize(), "and", Fruits[2].capitalize())
+veg = []
+veg.append(input("Enter a vegatable: "))
+veg.append(input("Enter a vegatable: "))
+veg.append(input("Enter a vegatable: "))
 
-while count != len(myList):
-    num += myList[count]
-    count += 1
+vegList = ("The list has the vegatables:", veg[0].capitalize(), veg[1].capitalize(), "and", veg[2].capitalize())
 
-print(num)
+food = []
+food.append(Fruits)
+food.append(veg)
+
+print(food)
+
+print()
