ea1f80f66ac6d7b6ca6ca2ab6b9457fcb4870307
zakkerkh <zakariyah.khan@hotmail.com>
2022-05-25 14:43:32 -0400

    init

---
+++ CS Class/HW15.py
@@ -0,0 +1,37 @@
+items = ['a','b','c','d']
+items.reverse()
+print(items)
+items = ['q','d','z','t']
+items.sort()
+print(items)
+items = [25,5,13,1]
+items.sort()
+print(items)
+# items['z','5','b',1]
+# items.sort()
+# print(items)
+items = ['a','b','c','d']
+items.clear()
+print(items)
+# items = ['a','b','c','d']
+# print(items.index('a'))
+# print(items.index('f'))
+items = ["me","you","us"]
+print("me" in items)
+print("myself" in items)
+items = [3,5,1,6,12]
+print(max(items))
+print(min(items))
+print(sum(items))
+# items = ['a','b','c','d']
+# print(max(items))
+# print(min(items))
+# print(sum(items))
+import random
+items = [1,2,6,3,5,8]
+random.choice(items)
+# import random
+# items = [1,2,6,3,5,8]
+# print(random.sample(items,2))
+# print(random.sample(items,5))
+# # print(random.sample(items,10))
