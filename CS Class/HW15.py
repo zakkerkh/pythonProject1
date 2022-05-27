544ae36f843b9b8ebc2fe563fab6a332f7255436
zakkerkh <zakariyah.khan@hotmail.com>
2022-05-27 14:43:41 -0400

    init

--- CS Class/HW15.py
+++ CS Class/HW15.py
@@ -101,24 +101,88 @@ import random
 #     print("False")
 
 #2
-num = int(input("Enter a number between 10 an 20"))
-num1 = int(input("Enter a number between 10 an 20"))
-list = []
-list1 = []
+# num = int(input("Enter a number between 10 an 20"))
+# num1 = int(input("Enter a number between 10 an 20"))
+# list = []
+# list1 = []
+#
+# if num > num1:
+#     count = 100
+# #    count1 = 0
+#     while count-100 != num:
+#         list.append(1+count)
+#         if count-100 != num1:
+#             list1.append(1+count/num1)
+#         count += 1
+#
+# if num < num1:
+#     count = 100
+#     count1 = 0
+#     while count-100 != num1:
+#         list.append(1+count)
+#         if count1 != num:
+#             list1.append(1+count/num)
+#             count1 += 1
+#         count += 1
+#
+# print(list)
+# print(list1)
 
-if num > num1:
-    count = 0
-    while count != num:
-        list.append(1+count)
-        while count != num1:
-            list1.append((1+count)/num1)
+#3
+# num = int(input("Enter a number between 1 and 5: "))
+# num1 = int(input("Enter a number between 1 and 5: "))
+#
+# list = []
+# list1 = []
+#
+# while len(list) != num:
+#     var = random.randint(10, 20)
+#     list.append(var)
+#
+# while len(list1) != num1:
+#     var = random.randint(10, 20)
+#     list.append(var)
+#
+# if list[0] == list1[0] or list[-1] == list1[-1]:
+#     print("Bingo")
+# else:
+#     print("False")
+
+#4
+# count = 5
+# list = []
+# while count != 0:
+#     list.append(input("Enter a word: "))
+#     count -= 1
+#
+# num = int(input("Enter a number: "))
+#
+# while count != 5:
+#     if len(list[count]) == num:
+#         print(list[count])
+#     count += 1
+
+#coding practice
+#J1
+list = []
+w_count = 0
+l_count = 0
+count = 0
+group = 0
+
+while count != 6:
+    entry = input("Enter W or L: ")
+    if entry == "W":
+        w_count += 1
+    else:
+        l_count += 1
     count += 1
-elif num < num1:
-    count = 0
-    while count != num1:
-        list.append(1+count)
-        while count != num:
-            list1.append((1+count)/num)
-    count += 1
-print(list)
-print(list1)
+
+if w_count >= 5:
+    print("1")
+elif w_count >= 3:
+    print("2")
+elif w_count >= 1:
+    print("3")
+else:
+    print("-1, Fatality")
