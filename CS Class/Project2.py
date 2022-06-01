d0deaa4a9f2488b22ec485838829f96350656ba6
zakkerkh <zakariyah.khan@hotmail.com>
2022-06-01 14:22:48 -0400

    init

--- CS Class/Project2.py
+++ CS Class/Project2.py
@@ -4,3 +4,29 @@
 #Hello, how are you doing
 
 
+# a-k
+# b-d
+# c-n
+# d-l
+# e-x
+# f-r
+# g-s
+# h-m
+# i-a
+# j-j
+# k-v
+# l-u
+# m-g
+# n-f
+# o-c
+# p-b
+# q-p
+# r-h
+# s-w
+# t-t
+# u-e
+# v-z
+# w-y
+# x-o
+# y-q
+# z-i
