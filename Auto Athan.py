7b2291afd00f016a03c3a1140705014ef4fd313c
zakkerkh <zakariyah.khan@hotmail.com>
2022-06-20 14:39:26 -0400

    init

--- Auto Athan.py
+++ Auto Athan.py
@@ -1,8 +1,22 @@
 import requests
+import json
 
 params = {"longitude": -79.556109, "latitude": 43.826185, "date_or_timestamp": "17-06-2022", "annual": False, "method": 2, "school": 0}
 # response = requests.get("http://api.aladhan.com/v1/calendar", params=params)
 
 response = requests.get("http://api.aladhan.com/v1/timings/:date_or_timestamp", params=params)
 
-print(response.json())
+athan = response.json()
+
+print(type(athan))
+
+thetime = athan['data']['timings']['Fajr']
+print(thetime)
+# new_string = json.dumps(athan, indent=2)
+# print(type(new_string))
+#
+# print("klfdjglfjlg")
+#
+# print(new_string)
+
+
